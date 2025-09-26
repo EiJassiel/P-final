becbdc5a132e9ce08439cc77e7b8241e67f9a683
jassiel <einer.mosquera@utp.ac.pa>
2025-09-26 13:48:24 -0500

    actulizaciones al proyecto

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hpelite-book840g4/proyecto-semestral',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c5f2408d31167a22fd95bbfb8fa5ddde85c0ea84',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hpelite-book840g4/proyecto-semestral' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c5f2408d31167a22fd95bbfb8fa5ddde85c0ea84',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
